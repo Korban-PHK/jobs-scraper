631a4a95ae9aae476fc7b36f7682944c31a742d7
rasyidcode <rasyidcode7@gmail.com>
2025-11-20 21:43:50 +0800

    sucessful selecting the job title

--- index.js
+++ index.js
@@ -7,9 +7,21 @@ await page.goto("https://id.jobstreet.com/id/PHP-Developer-jobs");
 
 await page.setViewport({ width: 1080, height: 1024 });
 
-const articleSelector = await page.locator("article").waitHandle();
-const jobId = await articleSelector?.evaluate((el) =>
-  el.getAttribute("data-job-id")
-);
+// await page.waitForSelector('article');
+// const jobId = await articleSelector?.evaluate((el) =>
+//   el.getAttribute("data-job-id")
+// );
+// const jobs = await articlesSelector?.evaluate((el) => el.classList);
+// const articles = await articlesSelector?.$$eval();
+// const jobCards = await page.$
+const articles = await page.$$("article");
+for (const article of articles) {
+  const title = await article.$eval('a[data-automation="jobTitle"]', (el) =>
+    el.textContent.trim()
+  );
+  console.log(title);
+}
 
-console.log(jobId);
+console.log("Found: ", articles.length);
+
+await browser.close();
